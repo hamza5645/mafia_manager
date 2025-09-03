fb4ec9f1eb88ff1648194dbc46c80217b1384045
hamza5645 <hamzaosama5645@outlook.com>
2025-09-04 00:13:15 +0300

    Initial Commit

---
+++ mafia_manager/mafia_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mafia_managerApp.swift
+//  mafia_manager
+//
+//  Created by Hamza Osama on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct mafia_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
